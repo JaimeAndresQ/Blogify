e721c287aef21ab7e88e979026abc22cb727694f
Jaime Andres Quiñones Chacón <jaime.quinones.chacon@gmail.com>
2021-10-28 22:50:13 -0500

    Adding git post component and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { CreatePostComponentComponent } from './create-post-component/create-post-component.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'create', component: CreatePostComponentComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreatePostComponentComponent } from './create-post-component/create-post-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreatePostComponentComponent
   ],
   imports: [
     BrowserModule,
